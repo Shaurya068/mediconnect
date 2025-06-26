8d55c531a307a41c900c377ec4f9a035c3c4dea5
Shaurya Dwivedi <164564564+Shaurya068@users.noreply.github.com>
2025-06-27 00:44:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/custom.css';
-=======
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
->>>>>>> d13b663af9b5931e3fe06efe53e7468f00822f26
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,11 +10,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-<<<<<<< HEAD
-=======
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
->>>>>>> d13b663af9b5931e3fe06efe53e7468f00822f26
